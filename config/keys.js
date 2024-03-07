8883fb22650a117675e747e06a0ef4869fa7de97
rohit <rohitiitian1235@gmail.com>
2024-03-08 00:53:33 +0530

    initial

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "298620116935-pq4ln11s2h389ik2nrtg7ptvoc17128o.apps.googleusercontent.com",
+  googleClientSecret: "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",
+  mongoURI:
+    "mongodb+srv://rohitniitian1235:rohit7734@cluster0.ymz6zen.mongodb.net/emailydb?retryWrites=true&w=majority&appName=Cluster0",
+  cookieKey: "jlkjfdklajfdajiejijijfdf",
+};
